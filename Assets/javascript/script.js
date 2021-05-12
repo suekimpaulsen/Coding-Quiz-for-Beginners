f8bcf7a7294f5a3bd32ff43a9eb95013df863cec
Sue Kim Paulsen <suekimpaulsen@gmail.com>
2021-05-12 13:15:26 -0500

    css js files with starting notes

---
+++ Assets/javascript/script.js
@@ -0,0 +1,35 @@
+// timer, start, questions, end, score
+
+// TIMER
+// var timer = document.getElementById('countdown');
+// function countdown() {
+//     var timeLeft = 75;
+//     var timeInterval = setInterval(function) {
+//         //TIMER CODE
+//     }, 1000);
+// }
+
+//QUESTIONS
+// var codeQuestions = document.getElementById("questions");
+// var codeQuestions = [
+//     { 
+//         question: "What tag is used to define a list item?",
+//         answers: {
+//             choice1: "<li>", 
+//             choice2: "<list>", 
+//             choice3: "<l>", 
+//             choice4: "<list item>",
+//             correctAnswer: "1"
+//         },
+//     }
+// ];
+
+//SCORE
+// var score = 0;
+// for (var i = 0; i < codeQuestions.length; i++) {
+//     var answer = 
+// }
+
+//START QUIZ
+
+//END QUIZ
