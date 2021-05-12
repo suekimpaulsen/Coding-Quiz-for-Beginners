650f1346684c38c85c5aef19ad82ded18e42f327
Sue Kim Paulsen <suekimpaulsen@gmail.com>
2021-05-12 18:41:50 -0500

    start button and timer added

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -1,15 +1,40 @@
-// timer, start, questions, end, score
+// timer, start, questions, end, name, score
 
+// START QUIZ
+var startBtn = document.querySelector('#start'); // == var starQuiz = document.getElementById("start");
+startBtn.addEventListener('click', startQuiz);
+
+function startQuiz() {
+    console.log("the game has started")
+    startTimer();
+}
+
+var timeLeftEl = document.getElementById("timeLeft")
+function startTimer() {
+    var timeLeft = 3;
+    var timeInterval = setInterval(function() {
+        if (timeLeft === 0) {
+            console.log("the count reached 0");
+            clearInterval(timeInterval);
+        }
+        else {
+            timeLeft--;
+            timeInterval.querySelector('#timeleft') = timeLeft;
+        }
+    }, 1000);
+}
 // TIMER
 // var timer = document.getElementById('countdown');
 // function countdown() {
 //     var timeLeft = 75;
 //     var timeInterval = setInterval(function) {
-//         //TIMER CODE
+            //TIMER CODE
+            // if Finishes the Quiz. timer stops
+            // if timer reaches 0, finishes the quiz
 //     }, 1000);
 // }
 
-//QUESTIONS
+// QUESTIONS
 // var codeQuestions = document.getElementById("questions");
 // var codeQuestions = [
 //     { 
@@ -30,6 +55,19 @@
 //     var answer = 
 // }
 
-//START QUIZ
+// END QUIZ
 
-//END QUIZ
+// NAME
+
+// VIEW HIGH SCORES
+
+// SCORES - GO BACK
+// SCORES - CLEAR HIGH SCORES
+
+
+// BUTTONS
+// - start quiz
+// - multiple choice options 
+// - submit 
+// - go back 
+// - clear high scores 
