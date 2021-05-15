ae3ca59c636200cc2732b8f777ec8fbf255b4015
Sue Kim Paulsen <suekimpaulsen@gmail.com>
2021-05-14 22:35:04 -0500

    questions array added

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -1,15 +1,72 @@
 // timer, start, questions, end, name, score
+var codeQuestions = [
+    { 
+        question: "What tag is used to define a list item?",
+        answers: {
+            choice1: "<li>", 
+            choice2: "<list>", 
+            choice3: "<l>", 
+            choice4: "<list item>",
+            correctAnswer: "1"
+        },
+    }
+    { 
+        question: "What is the name of the statement that is used to exit or end loop?",
+        answers: {
+            choice1: "Falter statement", 
+            choice2: "Close statement", 
+            choice3: "Conditional statement", 
+            choice4: "Break statement",
+            correctAnswer: "4"
+        },
+    }
+    { 
+        question: "The link element must go inside the ___ section of an HTML document or page.",
+        answers: {
+            choice1: "Footer", 
+            choice2: "Paragraph", 
+            choice3: "Head", 
+            choice4: "Body",
+            correctAnswer: "3"
+        },
+    }
+    { 
+        question: "What is a JavaScript element that represents either TRUE or FALSE values?",
+        answers: {
+            choice1: "Event", 
+            choice2: "Boolean", 
+            choice3: "RegExp", 
+            choice4: "Condition",
+            correctAnswer: "2"
+        },
+    }
+    { 
+        question: "External stylesheets are stored in what type of files?",
+        answers: {
+            choice1: "CSS", 
+            choice2: "PHP", 
+            choice3: "HTML", 
+            choice4: "XML",
+            correctAnswer: "1"
+        },
+    }
+];
+
 
 // START QUIZ
-var startBtn = document.querySelector('#start'); // == var starQuiz = document.getElementById("start");
+var mainEl = document.getElementById('container');
+var startBtn = document.getElementById('start'); // == var starQuiz = document.getElementById("start");
+console.log(startBtn)
 startBtn.addEventListener('click', startQuiz);
 
 function startQuiz() {
     console.log("the game has started")
     startTimer();
+    startQuestions();
+    startBtn.innerHTML = "";
 }
 
-var timeLeftEl = document.getElementById("timeLeft")
+var timeLeftEl = document.getElementById("timeleft")
 function startTimer() {
     var timeLeft = 3;
     var timeInterval = setInterval(function() {
@@ -18,36 +75,19 @@ function startTimer() {
             clearInterval(timeInterval);
         }
         else {
+            console.log(timeLeft)
             timeLeft--;
-            timeInterval.querySelector('#timeleft') = timeLeft;
+            timeLeftEl.innerHTML = 'Time Left: ' + timeLeft;
         }
     }, 1000);
 }
-// TIMER
-// var timer = document.getElementById('countdown');
-// function countdown() {
-//     var timeLeft = 75;
-//     var timeInterval = setInterval(function) {
-            //TIMER CODE
-            // if Finishes the Quiz. timer stops
-            // if timer reaches 0, finishes the quiz
-//     }, 1000);
+
+
+// var codeQuestions = document.getElementById("quiz");
+// function startQuestions() {
+//     console.log("questions")
 // }
 
-// QUESTIONS
-// var codeQuestions = document.getElementById("questions");
-// var codeQuestions = [
-//     { 
-//         question: "What tag is used to define a list item?",
-//         answers: {
-//             choice1: "<li>", 
-//             choice2: "<list>", 
-//             choice3: "<l>", 
-//             choice4: "<list item>",
-//             correctAnswer: "1"
-//         },
-//     }
-// ];
 
 //SCORE
 // var score = 0;
@@ -71,3 +111,16 @@ function startTimer() {
 // - submit 
 // - go back 
 // - clear high scores 
+
+
+// TIMER
+// var timer = document.getElementById('countdown');
+// function countdown() {
+//     var timeLeft = 75;
+//     var timeInterval = setInterval(function) {
+            //TIMER CODE
+            // if Finishes the Quiz. timer stops
+            // if timer reaches 0, finishes the quiz
+//     }, 1000);
+// }
+
