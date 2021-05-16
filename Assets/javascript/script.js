e0c2ba59ce53bffe7503df64dc68957ff7157b94
Sue Kim Paulsen <suekimpaulsen@gmail.com>
2021-05-15 19:02:00 -0500

    adding startQs

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -6,102 +6,108 @@ var choiceBtn4 = document.getElementById("choice4")
 
 var questionIndex = 0;
 var score = 0;
-// var codeQuestions = [
-//     { 
-//         question: "What tag is used to define a list item?",
-//         answers: {
-//             choice1: "<li>", 
-//             choice2: "<list>", 
-//             choice3: "<l>", 
-//             choice4: "<list item>",
-//             correctAnswer: "1"
-//         },
-//     },
-//     { 
-//         question: "What is the name of the statement that is used to exit or end loop?",
-//         answers: {
-//             choice1: "Falter statement", 
-//             choice2: "Close statement", 
-//             choice3: "Conditional statement", 
-//             choice4: "Break statement",
-//             correctAnswer: "4"
-//         },
-//     },
-//     { 
-//         question: "The link element must go inside the ___ section of an HTML document or page.",
-//         answers: {
-//             choice1: "Footer", 
-//             choice2: "Paragraph", 
-//             choice3: "Head", 
-//             choice4: "Body",
-//             correctAnswer: "3"
-//         },
-//     },
-//     { 
-//         question: "What is a JavaScript element that represents either TRUE or FALSE values?",
-//         answers: {
-//             choice1: "Event", 
-//             choice2: "Boolean", 
-//             choice3: "RegExp", 
-//             choice4: "Condition",
-//             correctAnswer: "2"
-//         },
-//     },
-//     { 
-//         question: "External stylesheets are stored in what type of files?",
-//         answers: {
-//             choice1: "CSS", 
-//             choice2: "PHP", 
-//             choice3: "HTML", 
-//             choice4: "XML",
-//             correctAnswer: "1"
-//         },
-//     }
-// ];
+var codeQuestions = [
+    { 
+        question: "What tag is used to define a list item?",
+        answers: {
+            choice1: "<li>", 
+            choice2: "<list>", 
+            choice3: "<l>", 
+            choice4: "<list item>",
+            correctAnswer: "1"
+        },
+    },
+    { 
+        question: "What is the name of the statement that is used to exit or end loop?",
+        answers: {
+            choice1: "Falter statement", 
+            choice2: "Close statement", 
+            choice3: "Conditional statement", 
+            choice4: "Break statement",
+            correctAnswer: "4"
+        },
+    },
+    { 
+        question: "The link element must go inside the ___ section of an HTML document or page.",
+        answers: {
+            choice1: "Footer", 
+            choice2: "Paragraph", 
+            choice3: "Head", 
+            choice4: "Body",
+            correctAnswer: "3"
+        },
+    },
+    { 
+        question: "What is a JavaScript element that represents either TRUE or FALSE values?",
+        answers: {
+            choice1: "Event", 
+            choice2: "Boolean", 
+            choice3: "RegExp", 
+            choice4: "Condition",
+            correctAnswer: "2"
+        },
+    },
+    { 
+        question: "External stylesheets are stored in what type of files?",
+        answers: {
+            choice1: "CSS", 
+            choice2: "PHP", 
+            choice3: "HTML", 
+            choice4: "XML",
+            correctAnswer: "1"
+        },
+    }
+];
 
-function startQuestions() {
-    console.log("quiz starts")
-}
 // START QUIZ
-var mainElement = document.getElementById('main');
+var contentElement = document.getElementById('q-content');
 var startBtn = document.getElementById('start'); // == var starQuiz = document.getElementById("start");
 console.log(startBtn)
 startBtn.addEventListener('click', startQuiz);
 
+function startQuestions() {
+    console.log("quiz starts")
+    if (questionIndex === codeQuestions.length) {
+        console.log ("quiz over")
+        // quizOver();
+    }
+    
+    
+}
+
 
 function startQuiz() {
-    // quizContainer.classList.add("hide")
+    // contentElement.classList.add("hide")
     console.log("the game has started")
     startTimer();
-    mainElement.innerHTML = "";
-    let questionElement = document.createElement("p");
-    questionElement.innerHTML = question1.question;
-    mainElement.append(questionElement);
+    // contentElement.innerHTML = "";
+    startQuestions();
+    // let questionElement = document.createElement('div');
+    // questionElement.setAttribute("div", "questions");
+    // contentElement.append(questionElement);
 }
-var question1 = {
-    question: "What tag is used to define a list item?",
-    answers: {
-        choice1: "<li>", 
-        choice2: "<list>", 
-        choice3: "<l>", 
-        choice4: "<list item>",
-        correctAnswer: "1"
-    }
-}
-
 var timeLeftEl = document.getElementById("timeleft")
 function startTimer() {
     var timeLeft = 3;
     var timeInterval = setInterval(function() {
-        if (timeLeft === 0) {
-            console.log("the count reached 0");
-            clearInterval(timeInterval);
-        }
-        else {
-            console.log(timeLeft)
+        if (timeLeft !== 0) {
             timeLeft--;
             timeLeftEl.innerHTML = 'Time Left: ' + timeLeft;
         }
+        else {
+            clearInterval(timeInterval);
+            console.log ("the count reached 0");
+            // quizOver();
+        }
+        // if (timeLeft === 0) {
+        //     console.log("the count reached 0");
+        //     clearInterval(timeInterval);
+        // }
+        // else {
+        //     console.log(timeLeft)
+        //     timeLeft--;
+        //     timeLeftEl.innerHTML = 'Time Left: ' + timeLeft;
+        // }
     }, 1000);
 }
 
